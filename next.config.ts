cbd2e795a8cc0b92f899da92e48a07010b9ac254
kengo-ut <keng1001uni@g.ecc.u-tokyo.ac.jp>
2025-02-18 08:06:00 +0900

    feat: deploy用の設定に変更

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '',
-  // output: 'export',
-  // images: { unoptimized: true },
+  output: 'export',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
