32b417cc054820c2789d4bd5d97c494a28d81f81
kengo <131339484+kengo-ut@users.noreply.github.com>
2025-06-13 20:31:12 +0900

    update: 画像ドメインの設定を追加

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export' を設定
   output: 'export',
-  images: { unoptimized: true },
+
+  // 画像を配信する外部ドメインを登録
+  images: {
+    unoptimized: true, // 画像の最適化を無効化
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
