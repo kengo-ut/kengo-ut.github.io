a45a704d8f5bd7c4fd13b3c38e165484e1f32bbb
kengo <131339484+kengo-ut@users.noreply.github.com>
2025-06-14 20:26:44 +0900

    fix: ビルドキャッシュを残さないよう修正

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export' を設定
   output: 'export',
-
-  // 画像を配信する外部ドメインを登録
-  images: {
-    unoptimized: true, // 画像の最適化を無効化
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
